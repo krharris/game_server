6c15d847d400ef68b3f7866e8f50fd566f2ba575
Kevin Harris <tokevinharris@gmail.com>
2012-02-11 16:13:39 -0600

    Add secure password system via SHA2.

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,6 +3,8 @@ class Game < ActiveRecord::Base
   has_many :chats
   has_many :moves
   
+  # Allow the two users known in the table as 'user_id_1' and 'user_id_2' to
+  # be accessed as 'user1' and 'user2'.
   belongs_to :user1, :class_name => 'User', :foreign_key => 'user_id_1'
   belongs_to :user2, :class_name => 'User', :foreign_key => 'user_id_2'
   
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,11 @@ class User < ActiveRecord::Base
   has_many :moves
   has_many :chats
 
-  #attr_accessor :password
-  attr_accessible :name, :email
+  # :password and :password_confirmation do not exist in the table but are 
+  # manaully added to each User object created.
+  attr_accessor :password
+  
+  attr_accessible :name, :email, :password, :password_confirmation
   
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   
@@ -15,4 +18,44 @@ class User < ActiveRecord::Base
   validates :email, :presence   => true,
                     :format     => { :with => email_regex },
                     :uniqueness => { :case_sensitive => false }
+                    
+  validates :password, :presence     => true,
+                       :confirmation => true, # This will force the creation of :password_confirmation
+                       :length       => { :within => 6..40 }
+                       
+  before_save :encrypt_password
+  
+  def has_password?(submitted_password)
+    self.encrypted_password == encrypt(submitted_password)
+  end
+  
+  class << self
+    
+    def authenticate(email, submitted_password)
+      user = find_by_email(email)
+      return nil if user.nil?
+      return user if user.has_password?(submitted_password)
+    end
+    
+  end
+  
+  private
+  
+    def encrypt_password
+      self.salt = make_salt if self.new_record?
+      self.encrypted_password = encrypt( self.password )
+    end
+    
+    def encrypt(string)
+      secure_hash("#{self.salt}--#{string}")
+    end
+    
+    def make_salt
+      secure_hash("#{Time.now.utc}--#{self.password}")
+    end
+    
+    def secure_hash(string)
+      Digest::SHA2.hexdigest(string)
+    end
+  
 end
