253f64e103444b51c790c2c03629088055eb87b6
Kevin Harris <tokevinharris@gmail.com>
2012-03-29 21:14:57 -0500

    Add uniqueness to name.

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,8 @@ class User < ActiveRecord::Base
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   
   validates :name, :presence => true,
-                   :length   => { :maximum => 50 }
+                   :length   => { :maximum => 50 },
+                   :uniqueness => { :case_sensitive => false }
   
   validates :email, :presence   => true,
                     :format     => { :with => email_regex },
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120211041720) do
+ActiveRecord::Schema.define(:version => 20120330020359) do
 
   create_table "chats", :force => true do |t|
     t.integer  "user_id"
@@ -56,5 +56,6 @@ ActiveRecord::Schema.define(:version => 20120211041720) do
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["name"], :name => "index_users_on_name", :unique => true
 
 end
